f1f9c4182f81049dbe9a74eebf135bca898df495
Matias Salibian-Barrera <matias@stat.ubc.ca>
2020-05-12 10:11:17 -0700

    added package.R, included @import

---
+++ R/sparseFPCA.R
@@ -0,0 +1,15 @@
+
+#' sparseFPCA: A package for computating the notorious bar statistic.
+#'
+#' The foo package provides three categories of important functions:
+#' foo, bar and baz.
+#'
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name sparseFPCA
+NULL
+
+#' @import mgcv doParallel
+NULL
